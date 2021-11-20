dcc8bf6f0774717d7c97e9e44a7518b0ff67be27
alejandrofdez-us <afdez@us.es>
2021-11-20 08:58:30 +0100

    Solution for lab1 (restaurant migration and restaurant model)

--- models/restaurant.js
+++ models/restaurant.js
@@ -30,8 +30,38 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Restaurant.init({
-    //TODO: Include the rest of the properties of the Restaurant model
-
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    address: DataTypes.STRING,
+    postalCode: DataTypes.STRING,
+    url: DataTypes.STRING,
+    shippingCosts: DataTypes.DOUBLE,
+    averageServiceMinutes: DataTypes.DOUBLE,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    logo: DataTypes.STRING,
+    heroImage: DataTypes.STRING,
+    status: {
+      type: DataTypes.ENUM,
+      values: [
+        'online',
+        'offline',
+        'closed',
+        'temporarily closed'
+      ]
+    },
+    restaurantCategoryId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    }
   }, {
     sequelize,
     modelName: 'Restaurant'
